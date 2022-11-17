e29ecbaaa0dbd00f34ad5da459bcc51ffa812c52
mtf360 <metamorfosismarketing360@gmail.com>
2022-11-16 22:52:18 -0300

    email client added

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,14 @@
-// path: ./config/plugins.js
-
 module.exports = ({ env }) => ({
-    // enable a plugin that doesn't require any configuration
-    i18n: true,
-    // enable a custom plugin
-    // myplugin: {
-    // my-plugin is going to be the internal name used for this plugin
-    // enabled: true,
-    // resolve: './src/plugins/my-local-plugin',
-    // config: {
-        // user plugin config goes here
-    // },
-    // },
-
-    // disable a plugin
-    // myotherplugin: {
-    // enabled: false, // plugin installed but disabled
-    // },
+    email: {
+    config: {
+        provider: 'sendgrid',
+        providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+        },
+        settings: {
+        defaultFrom: 'soporte@arpitools.com',
+        defaultReplyTo: 'soporte@arpitools.com',
+        },
+    },
+    },
 });
