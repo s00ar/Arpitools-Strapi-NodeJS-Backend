dd527714352ef1dc278d9a1528d35c81aa8ba1a0
ebraheemijaz <ebraheemijaz913@gmail.com>
2022-11-24 14:30:29 +0500

    added seller total amount in sepecifc order id range

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', '82.180.130.53'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'api'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'arpitools_api'),
+      user: env('DATABASE_USERNAME', 'arpitools_root'),
+      password: env('DATABASE_PASSWORD', 'Lks534%1'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -6,4 +6,25 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+    async find(ctx) {
+        ctx.query = { ...ctx.query };
+        const { data, meta } = await super.find(ctx);
+        meta.date = Date.now();
+
+        let startingRange = ctx.query.filters?.id?.$gte?.[0]
+        let endingRange = ctx.query.filters?.id?.$lte?.[0]
+        let sellers_amount = []
+        if (startingRange && endingRange) {
+            sellers_amount = await strapi.db.connection.raw(`
+                SELECT  seller_id,sum(amount),name FROM arpitools_api.orders_seller_links
+                INNER JOIN orders ON orders.id=orders_seller_links.order_id
+                INNER JOIN sellers ON sellers.id=orders_seller_links.seller_id
+                where order_id >= ${startingRange} and order_id <= ${endingRange}
+                GROUP BY orders_seller_links.seller_id
+            `)
+            sellers_amount = sellers_amount[0] ? JSON.parse(JSON.stringify(sellers_amount[0])) : []
+        }
+        return { data, meta, sellers_amount };
+    }
+}));
